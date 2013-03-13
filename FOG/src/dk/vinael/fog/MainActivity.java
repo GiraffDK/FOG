a7ced467e3a197b88d8d740c85aa05e3c611ae19
Mikael Vanggaard Jensen <Mikael.vang@gmail.com>
2013-03-13 20:37:12 +0100

    Startpage er lavet med 4 knapper og en registreret events.

--- FOG/src/dk/vinael/fog/MainActivity.java
+++ FOG/src/dk/vinael/fog/MainActivity.java
@@ -1,15 +1,37 @@
 package dk.vinael.fog;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	ArrayList<String> items = new ArrayList<String>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		items.add("Nizzle Party1");
+		items.add("Nizzle Party2");
+		items.add("Nizzle Party3");
+		items.add("Nizzle Party1");
+		items.add("Nizzle Party2");
+		items.add("Nizzle Party3");
+		items.add("Nizzle Party1");
+		items.add("Nizzle Party2");
+		items.add("Nizzle Party3");
+		items.add("Nizzle Party1");
+		items.add("Nizzle Party2");
+		items.add("Nizzle Party3");
+		ListView lv = (ListView)	findViewById(R.id.listView1);
+		ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+		lv.setAdapter(myarrayAdapter);
+		lv.setTextFilterEnabled(true);
 	}
 
 	@Override
