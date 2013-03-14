8b49301287f05fa9bfc7bb0fbe73acfe21e24b73
Mikael Vanggaard Jensen <Mikael.vang@gmail.com>
2013-03-14 15:02:56 +0100

    drawables files

--- FOG/gen/dk/vinael/fog/R.java
+++ FOG/gen/dk/vinael/fog/R.java
@@ -22,13 +22,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_add=0x7f020000;
-        public static final int ic_event=0x7f020001;
-        public static final int ic_history=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int ic_news=0x7f020004;
-        public static final int ic_party=0x7f020005;
-        public static final int ic_search=0x7f020006;
-        public static final int ic_settings=0x7f020007;
+        public static final int ic_addevent=0x7f020001;
+        public static final int ic_event=0x7f020002;
+        public static final int ic_history=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_news=0x7f020005;
+        public static final int ic_newsicon=0x7f020006;
+        public static final int ic_party=0x7f020007;
+        public static final int ic_profile=0x7f020008;
+        public static final int ic_search=0x7f020009;
+        public static final int ic_settings=0x7f02000a;
+        public static final int ic_socialparty=0x7f02000b;
     }
     public static final class id {
         public static final int action_settings=0x7f08000e;
